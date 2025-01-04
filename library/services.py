c8d840fab64eccc666c3381fb2f4c4612e11b931
sergeytashkinov <tashkinov2@gmail.com>
2025-01-04 22:56:42 +0300

    fixed=2

--- library/services.py
+++ library/services.py
@@ -7,4 +7,6 @@ def send_telegram_message(chat_id, message):
         "text": message,
         "chat_id": chat_id,
     }
-    requests.get(f"{TELEGRAM_URL}{BOT_TOKEN}/send_message", params=params)
+    response = requests.get(f"{TELEGRAM_URL}{BOT_TOKEN}/send_message", params=params)
+    if response.status_code != 200:
+        send_telegram_message(chat_id, message)
